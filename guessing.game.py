96bfa6e471ee93eecc6523d2abccf2d0911d43f2
Leshani318 <dlhimasha@gmail.com>
2025-05-03 21:17:58 +0530

    Added hint if guess is high or low guess

--- guessing.game.py
+++ guessing.game.py
@@ -1,9 +1,19 @@
 import random
-number = random.randint(1, 10)
-print("Guess a number between 1 and 10")
+import time
+start_time = time.time()
+while True:
+number = random.randint(1, 100)
+print("Guess a number between 1 and 100")
+attempts = 0
+attempts +=1
 guess = int(input())
 
 if guess == number:
     print("You win!")
 else:
     print(f"Wrong! The number was {number}")
+    print("Play again? (y/n)")
+      if input().lower() != 'y':
+       break
+    print(f"You made {attempts} attempts. ")
+    print(f"Time taken: {time.time() - start_time: .2f}s")
